0319c84c2b6bf2378f477608de4e6dd4b87e9963
Tobin <tobinjbrown@gmail.com>
2015-03-01 22:53:54 -0600

    I AM GROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOT

--- tree.py
+++ tree.py
@@ -11,6 +11,7 @@ list of filenames will be as long as the fanout
 import struct
 import numpy
 import glob
+import time
 
 
 from sort import *
@@ -105,9 +106,41 @@ def write_user_node(files, states, name, layer):
 # work all the way down to the user files and return
 # the count of users
 def tree_nebraskans(filename):
-	return None
+	result = 0
+
+	if "tree" in filename:
+
+		root = open(filename, 'rb')
+		number = struct.unpack('i', root.read(4))[0]
+
+		content = []
+		for x in range(0, number):
+			content.append(struct.unpack('64s', root.read(64))[0].split('\x00', 1)[0])
+
+		i = 1
+		search = []
+		found = False
+		state = content[i]
+
+		# while we haven't found Nebraska, skip to the next state
+		while (i < len(content)-2) or (state == "Nebraska"):
+			if (state > "Nebraska" and not found) or (state == "Nebraska" and found):
+				# if we find Nebraska, add teh preceding file to the search list
+				before = content[i-1]
+				if before not in search:
+					search.append(before)
+				if not found:
+					found = True
+			# if the state is Nebraska, add it to the result
+			if state == 'Nebraska':
+				result += 1
+			i = i + 2
+			state = content[i]
+
+		# recursively search each file for Nebraskans
+		for file in search:
+			result += tree_nebraskans(file)
+
+	return result
 
 
-
-users = load_users()
-print(build_user_tree(users, 100))
