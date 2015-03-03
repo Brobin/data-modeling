0ef5f49f9aa07f665615c121ea637079e819b235
Tobin <tobinjbrown@gmail.com>
2015-03-03 11:29:28 -0600

    clean up models

--- models.py
+++ models.py
@@ -9,30 +9,26 @@ import struct
 
 
 class User():
-	def __init__(self, id, name, city, state, messages = [], children = 0):
+	def __init__(self, id, name, city, state, messages = []):
 		self.id = id
 		self.name = name
 		self.city = city
 		self.state = state
 		self.messages = messages
-		self.children = children
 	def __str__(self):
-		return self.csv()
-	def csv(self):
-		return "{0};{1};{2},{3}".format(self.id, self.name, self.city, self.state)
+		return "{0};{1};{2},{3}".format(self.id, 
+			self.name, self.city, self.state)
 	def byte(self):
-		return struct.pack('i64s32s32si', self.id, self.name, self.city, self.state, self.children)
+		return struct.pack('i64s32s32s', self.id, 
+			self.name, self.city, self.state)
 
 class Message():
-	def __init__(self, id, user_id, date, text, children = 0):
+	def __init__(self, id, user_id, date, text):
 		self.id = id
 		self.date = date
 		self.text = text
 		self.user_id = user_id
-		self.children = children
 	def __str__(self):
-		return self.csv()
-	def csv(self):
 		return "{0};{1}".format(self.id, self.date)
 	def byte(self):
 		year = self.date.year
@@ -40,6 +36,6 @@ class Message():
 		day = self.date.day
 		hour = self.date.hour
 		minute = self.date.minute
-		return struct.pack('iiiiiii1024si', self.id, self.user_id, 
-			year, month, day, hour, minute, self.text, self.children)
+		return struct.pack('iiiiiii1024s', self.id, self.user_id, 
+			year, month, day, hour, minute, self.text)
 
